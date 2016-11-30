e9df02227bb8efeca8dbe575a0971018207a02c5
email <g.julia82@gmail.com>
2016-11-30 22:43:48 +0200

    Initial commit

---
+++ src/main/java/SeleniumDemo.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.List;
+
+public class SeleniumDemo {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.bing.com/");
+        WebElement go = driver.findElement(By.name("go"));
+        go.sendKeys("automation");
+        go.click();
+        System.out.println(driver.getTitle());
+        List<WebElement> a = driver.findElements(By.xpath("//div[@class='b_title']/h2/a"));
+        for (WebElement el : a) {System.out.println(el.getText());
+        }
+    driver.quit();
+    }
+}
+
+
+
+
